c2566f1787cf471ce27d427a3c3b2b96e0c20811
shahj <shahj@DESKTOP-15TQQFB>
2023-10-08 21:30:41 -0400

    Add project files.

---
+++ Lab1/Models/Employer.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+
+namespace Lab1.Models
+{
+    public class Employer
+    {
+        //Value is a unique identifier 
+        [Key]
+        public int Id { get; set; }
+        //Name of the person 
+        [Required]
+        public string Name { get; set; }
+        //Phone number of the person
+        [Required, Phone, Display(Name = "Phone Number")]
+        public string PhoneNumber { get; set; }
+        //Website of the person
+        [Required, Url]
+        public string Website {  get; set; }
+        //Incorporated Date (Honestly don't know what this is supposed to mean? I assumed it meant start date)
+        [DataType(DataType.Date), Display(Name = "Incorporated Date")]
+        public DateTime? IncorporatedDate { get; set; }
+        
+    }
+}
